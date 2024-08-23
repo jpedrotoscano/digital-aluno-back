1a494acab2d7166706c35ebc70cc44940ee6800f
jpedrotoscano <jpedro.toscano03@gmail.com>
2024-08-23 10:57:55 -0300

    Fix: Campo topico adicionado a model reposicao

---
+++ src/routes/reposicaoRoutes.js
@@ -0,0 +1,25 @@
+const { criarReposicao, listarReposicoes, listarReposicao, editarReposicao, deletarReposicao } = require("../controllers/reposicaoController")
+
+const router = require("express").Router()
+
+router.post("/", async (req, res) => {
+    res.send(await criarReposicao(req.body))
+})
+
+router.get("/", async (req, res) => {
+    res.send(await listarReposicoes())
+})
+
+router.get("/:id", async (req, res) => {
+    res.send(await listarReposicao(req.params.id))
+})
+
+router.post("/:id", async (req, res) => {
+    res.send(await editarReposicao(req.params.id, req.body))
+})
+
+router.delete("/:id", async (req, res) => {
+    res.send(await deletarReposicao(req.params.id))
+})
+
+module.exports = router
